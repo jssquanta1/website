70aab2f2dad5a3141c6a46ce4ffcdb35a97b5377
akash <anandakash656@gmail.com>
2024-05-19 10:14:00 +0530

    changed the changed the events pics and year of zealicon section

--- src/Pages/Zealicon.jsx
+++ src/Pages/Zealicon.jsx
@@ -4,14 +4,14 @@ import ZealiconFooter from "../Components/ZealiconFooter";
 import ZealiconNavbar from "../Components/ZealiconNavbar";
 import ZeliconHero from "../Components/ZealiconHero";
 import ZeliconEvents from "../Components/ZeliconEvents";
-// import ZealiconWinner from "../Components/ZealiconWinner";
+import ZealiconWinner from "../Components/ZealiconWinner";
 import ZealiconCongratulation from "../Components/ZealiconCongratulation";
 export default function Zealicon() {
   return (
     <div className={styles.main}>
       <ZealiconNavbar />
       <ZeliconHero />
-      <ZealiconWinner />
+      {/* <ZealiconWinner /> */}
       <ZealiconCongratulation />
       {/* <ZeliconEvents/> */}
       {/* <Poll /> */}
